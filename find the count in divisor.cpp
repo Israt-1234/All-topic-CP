5fd82f07362218d109f45f3ecb77572b7d2074c6
Israt jahan <140290748+Israt-1234@users.noreply.github.com>
2023-11-03 01:05:40 +0600

    Add files via upload

---
+++ find the count in divisor.cpp
@@ -0,0 +1,23 @@
+//1 to n divisor
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int n=5;
+ int d[n];
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+
+    for(int i=1;i<n;i++){
+        for(int j=i;j<n;j+=i){
+            d[j]++;
+            cout<<d[j]<<"d"<<endl;
+        }
+    }
+        for(int i=1;i<n;i++)
+    cout<<d[i]<<endl;
+    return 0;
+
+}
