d9188f0acd59c61fadeb7451e703a7390def741a
Israt Jahan <140290748+Israt-1234@users.noreply.github.com>
2023-12-12 10:35:52 +0600

    Contribution Technique

---
+++ Product of pair product.cpp
@@ -0,0 +1,22 @@
+/*........Hard work and consistency is the only way to success........ */
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int N=1005;
+//int d[N][N],pref[N][N];
+int main()
+{
+    // clock_t st= clock();
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    cin>>a[i];
+    int sum=1;
+    for(int i=0;i<n;i++){
+        sum*=pow(a[i],2*n);
+    }
+    cout<<sum<<'\n';
+    return 0;
+}
